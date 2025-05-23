5403c4d5de5bd2a9082af20383a1e343ce6e1371
EmilyGG24308 <gon24308@uvg.edu.gt>
2025-05-22 22:05:10 -0600

    Update Floyd.java

--- Floyd.java
+++ Floyd.java
@@ -1,1 +1,44 @@
+public class Floyd {
 
+    public static int[][] floydWarshall(int[][] grafo) {
+        int n = grafo.length;
+        int[][] dist = new int[n][n];
+
+        
+        for (int i = 0; i < n; i++)
+            System.arraycopy(grafo[i], 0, dist[i], 0, n);
+        
+            //algo. de floyd
+        for (int k = 0; k < n; k++)
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    if (dist[i][k] + dist[k][j] < dist[i][j])
+                        dist[i][j] = dist[i][k] + dist[k][j];
+
+        return dist;
+    }
+
+    //centro del grafo (node with menor exentricidad <mayor distancia mas corta>)
+    public static int centroDelGrafo(int[][] distancias) {
+        int n = distancias.length;
+        int[] excentricidades = new int[n];
+
+        //exentricidad de cada nodo (max desde uno al otro)
+        for (int i = 0; i < n; i++) {
+            int max = 0;
+            for (int j = 0; j < n; j++) {
+                if (distancias[i][j] != 99999 && distancias[i][j] > max)
+                    max = distancias[i][j];
+            }
+            excentricidades[i] = max;
+        }
+
+        //finds node with menor
+        int centro = 0;
+        for (int i = 1; i < n; i++)
+            if (excentricidades[i] < excentricidades[centro])
+                centro = i;
+
+        return centro;
+    }
+}
