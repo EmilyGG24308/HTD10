3b356cac881ff03d47d7085194ccc61aca439022
EmilyGG24308 <gon24308@uvg.edu.gt>
2025-05-22 22:04:49 -0600

    Update Grafo.java

--- Grafo.java
+++ Grafo.java
@@ -1,1 +1,41 @@
+import java.util.Arrays;
 
+public class Grafo{
+    //matriz d adyacencia q va a representar la dis
+    private int[][] matriz;
+    private final int INF = 99999;
+    private String[] vertices = {
+        "Ciudad de Guatemala", // A
+        "Zacapa",              // B
+        "Chiquimula",          // C
+        "Quetzaltenango",      // D
+        "Coban"                // E
+    };
+
+    
+    public Grafo() {
+        matriz = new int [5][5];
+        for (int i=0; i<5; i++)
+        Arrays.fill(matriz[i], INF); //begining disconected
+        for (int i=0; i<5; i++)
+        matriz[i][i] = 0; // a-->a = 0
+    }
+
+    //adds arco: origen --> destino con el km
+    public void agregarArco(int origen, int destino, int peso) {
+        matriz[origen][destino] = peso;
+    }
+
+    //elimina : distancia entre origen y destino a infinity
+    public void eliminarArco(int origen, int destino) {
+        matriz[origen][destino] = INF;
+    }
+
+    public int[][] getMatriz() {
+        return matriz;
+    }
+
+    public String[] getVertices() {
+        return vertices;
+    }
+}
